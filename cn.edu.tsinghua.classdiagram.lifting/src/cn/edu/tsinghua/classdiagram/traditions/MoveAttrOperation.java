ad1b3601535e001b24b7a1cfee4856cbaefad6bc
anyrenwei <anyrenwei@163.com>
2015-11-06 01:38:46 +0800

    tradition

---
+++ cn.edu.tsinghua.classdiagram.lifting/src/cn/edu/tsinghua/classdiagram/traditions/MoveAttrOperation.java
@@ -0,0 +1,19 @@
+package cn.edu.tsinghua.classdiagram.traditions;
+
+
+public class MoveAttrOperation {
+
+	
+	private String fromClass;
+	
+	private String toClass;
+	
+	private String attrName;
+	
+	private final Class<?>[] subOperations = { AddAttrOperation.class, DeleteAttrOperation.class};
+	
+	
+	
+	 
+
+}
--- cn.edu.tsinghua.classdiagram.lifting/src/cn/edu/tsinghua/classdiagram/traditions/PullUpAttrOperation.java
+++ cn.edu.tsinghua.classdiagram.lifting/src/cn/edu/tsinghua/classdiagram/traditions/PullUpAttrOperation.java
@@ -1,6 +1,48 @@
 package cn.edu.tsinghua.classdiagram.traditions;
 
-public class PullUpAttrOperation {
+import java.util.List;
+
+import cn.edu.tsinghua.classdiagram.diagram.Diagram;
+import cn.edu.tsinghua.classdiagram.operation.AtomicOperation;
+import cn.edu.tsinghua.classdiagram.operation.CompositeOperation;
+import cn.edu.tsinghua.classdiagram.operation.Operation;
+
+public class PullUpAttrOperation extends CompositeOperation{
+
+	
+	private final Class<?>[] liftOperations = { AddAttribute.class, PullUpAttrOperation.class, PushDownAttrOperation.class };
+
+	
+	
+	@Override
+	public void initSubOperations() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public CompositeOperation generateByMains(List<Operation> mains) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean searchOthers(List<Operation> sequence, List<Integer> resultIndexs) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Diagram execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void setRelatedElements() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 }
